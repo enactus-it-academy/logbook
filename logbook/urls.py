902f20e35d8ffa1d5cd3599f5c4e55bad9cd28f7
0muraliev <omuraliev2019@gmail.com>
2021-03-30 20:14:59 +0600

    Add Feedback serializers

--- logbook/urls.py
+++ logbook/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from communication import views
+
+router = DefaultRouter()
+router.register(r'feedbacks', views.FeedbackViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
