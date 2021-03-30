b98e5f67caf16fa6d4b168ad5218951aac237d40
0muraliev <omuraliev2019@gmail.com>
2021-03-30 19:51:44 +0600

    Add FeedbackAdmin

--- communication/admin.py
+++ communication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Feedback
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    model = Feedback
+    list_display = ['name', 'phone_number', 'date']
