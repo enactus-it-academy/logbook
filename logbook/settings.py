c6ff9da8ebd661457eed309069e8c97d889814cf
0muraliev <omuraliev2019@gmail.com>
2021-04-20 21:28:43 +0600

    Add variables to settings

--- logbook/settings.py
+++ logbook/settings.py
@@ -138,8 +138,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Django storages
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+if 'AWS_ACCESS_KEY_ID' in os.environ:
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
 
 
 # REST Auth
