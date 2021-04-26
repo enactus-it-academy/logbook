db7edd9fe6609e41c03369cc27a53d01b86f1305
0muraliev <omuraliev2019@gmail.com>
2021-04-26 17:30:48 +0600

    Add related_name to the owner field of the Product model

--- product/models.py
+++ product/models.py
@@ -1,26 +1,25 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Product(models.Model):
     objects = None
-    name = models.CharField(max_length=50, verbose_name="Название товара")
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Владелец")
-    sale_price = models.IntegerField(default=0, verbose_name="Цена на продажу")
-    cost_price = models.IntegerField(default=0, verbose_name="Себестоимость")
-    count = models.IntegerField(default=0, verbose_name="Количество купленных товаров")
-    count_sold = models.IntegerField(default=0, verbose_name="Проданные товары")
-    date = models.DateTimeField(verbose_name="Дата покупки товара", auto_now_add=True)
-    available_count = models.IntegerField(default=0, verbose_name="Остаток товаров", editable=False)
-    earns = models.IntegerField(default=0, verbose_name="Выручка с товара", editable=False)
-    total_earns = models.IntegerField(default=0, verbose_name="Выручка", editable=False)
+    name = models.CharField("Название товара", max_length=50)
+    owner = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              verbose_name="Владелец",
+                              related_name='products')
+    sale_price = models.IntegerField("Цена на продажу", default=0)
+    cost_price = models.IntegerField("Себестоимость", default=0)
+    count = models.IntegerField("Количество купленных товаров", default=0)
+    count_sold = models.IntegerField("Проданные товары", default=0)
+    date = models.DateTimeField("Дата покупки товара", auto_now_add=True)
+    available_count = models.IntegerField("Остаток товаров", default=0, editable=False)
+    earns = models.IntegerField("Выручка с товара", default=0, editable=False)
+    total_earns = models.IntegerField("Выручка", default=0, editable=False)
 
     def save(self, *args, **kwargs):
         self.available_count = self.count - self.count_sold
         self.earns = self.sale_price - self.cost_price
         self.total_earns = self.earns * self.count_sold
         super(Product, self).save(*args, **kwargs)
-
-    class Meta:
-        verbose_name = "товар"
-        verbose_name_plural = "Товары"
