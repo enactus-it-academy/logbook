24e2f3a30d1f1add117f8bdddeea8090a565a8b9
0muraliev <omuraliev2019@gmail.com>
2021-04-26 19:06:03 +0600

    Add "rest_framework.authentication.BasicAuthentication" to DEFAULT_AUTHENTICATION_CLASSES

--- logbook/settings.py
+++ logbook/settings.py
@@ -150,6 +150,7 @@ if 'AWS_ACCESS_KEY_ID' in os.environ:
 # REST Auth
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.BasicAuthentication',
+                                       'knox.auth.TokenAuthentication'),
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
 }
