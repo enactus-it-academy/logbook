0875c7302866d3f05a66f0dd1a8bc9acec030f42
0muraliev <omuraliev2019@gmail.com>
2021-05-03 12:22:22 +0600

    Delete fields from Product model

--- product/models.py
+++ product/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Product(models.Model):
-    objects = None
     name = models.CharField("Название товара", max_length=50)
     owner = models.ForeignKey(User,
                               on_delete=models.CASCADE,
@@ -14,6 +13,3 @@ class Product(models.Model):
     count = models.IntegerField("Количество купленных товаров", default=0)
     count_sold = models.IntegerField("Проданные товары", default=0)
     date = models.DateTimeField("Дата покупки товара", auto_now_add=True)
-    available_count = models.IntegerField("Остаток товаров", default=0, editable=False)
-    earns = models.IntegerField("Выручка с товара", default=0, editable=False)
-    total_earns = models.IntegerField("Выручка", default=0, editable=False)
