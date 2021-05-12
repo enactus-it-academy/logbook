a7dfa821b20292f16abee41bd1cc4bf2f00499c2
0muraliev <omuraliev2019@gmail.com>
2021-05-12 18:01:04 +0600

    Remove related name

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Store(models.Model):
     name = models.CharField(max_length=255)
-    owner = models.OneToOneField('account.Owner', on_delete=models.CASCADE, related_name='stores')
+    owner = models.OneToOneField('account.Owner', on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
