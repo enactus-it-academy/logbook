18d84e696a062335c96723756591daf0fa148dca
0muraliev <omuraliev2019@gmail.com>
2021-05-21 13:11:13 +0600

    Add blank to name field

--- product/models.py
+++ product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Store(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
     owner = models.OneToOneField('account.Owner', on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
